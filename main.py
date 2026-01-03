f479606fcd93dd5102a4c8e966c70f47329537fa
Callum Friedman <callum@cdf-mba.local>
2026-01-03 19:25:54 +0000

    Main application handles config loading
    - Load config parameters from environment variables if available
    - Load config parameters from YAML file

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
+import os
 from sseclient import SSEClient
-from config import config
+import yaml
+from dotenv import load_dotenv
 
-USER_AGENT = 'etl-wikipedia-updates/0.1-dev (contact: 43573338+cdfrdmn@users.noreply.github.com)'
-
-def sse_stream_iterator(url):
+def sse_stream_iterator(url, user_agent):
     """
     Connect to an HTTP SSE stream server and iterate over received messages.
 
     :param url: The URL of the desired SSE stream source.
+    :param url: The identifier of the application requesting the stream data (the default python user agent is usually blocked by a 403).
     """
-    request_headers = {'User-Agent': USER_AGENT} # Include an identifier in the request header to prevent a 403
+    request_headers = {'User-Agent': user_agent}
     messages = SSEClient(url, headers=request_headers)
 
     for message in messages:
         print(message)
 
+def load_config(config_path='config.yaml'):
+    """
+    Load static config from YAML and dynamic config from environment.
+    """
+    # Load environment variables from local .env file if available
+    load_dotenv()
+
+    # Load YAML config file
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Config file not found: {config_path}")
+    
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    
+    # Combine into a single Python object
+    config_dict = {
+        'user-agent': os.getenv('ETL_USER_AGENT'),
+        'stream-url': config.get('stream-url')
+    }
+    
+    return config_dict
+
 def main():
-    sse_stream_iterator(config.WIKI_STREAM_URL)
+    config = load_config()
+    sse_stream_iterator(config['stream-url'], config['user-agent'])
 
 if __name__ == "__main__":
     main()
